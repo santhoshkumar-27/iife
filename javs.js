fe1a15bb901fe22db073485704499ef648695d6e
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-12 20:04:04 +0530

    feat: wtihout annonyms functins

--- javs.js
+++ javs.js
@@ -1,9 +1,11 @@
 // named functions
 (function myFunc() {
     // Beginning of function
-    console.log('greeting');
+    console.log('named functions');
 })();
 
 
 // arrow functions
-(() => console.log('asdfads'))();
+(() => console.log('arrow functions'))();
+
+(function (){ console.log('annonyms functions')})();
