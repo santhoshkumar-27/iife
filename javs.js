ad059137af15f51c929779b230e6d2c724c94f80
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-12 20:07:06 +0530

    feat: global access variable

--- javs.js
+++ javs.js
@@ -1,9 +1,10 @@
 // https://circleci.com/blog/ci-cd-for-js-iifes/#prerequisites
+const greeting = "Hello world";
 
 // named functions
 (function myFunc() {
     // Beginning of function
-    console.log('named functions');
+    console.log(greeting);
 })();
 
 
